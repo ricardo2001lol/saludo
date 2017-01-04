7605d9cc80557013364d9d6db23c5bebed66340b
ricardo2001lol <ricardoluis2001@hotmail.com>
2017-01-04 09:12:16 -0400

    Add files via upload

--- Saludo.java
+++ Saludo.java
@@ -1,15 +1,17 @@
+/* IMPORTANTE: TIENES QUE TENER JAVA PARA PODER EJECUTAR ESTE PROGRAMA */
 import java.util.Scanner;
 
-public class Main
+// Saluda al nombre que ingresa el usuario
+public class Saludo
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Introduzca su nombre: ");
+		System.out.println("Introduzca su nombre: ");// Se piden los datos ql usuario
 		
-		String entradaTeclado = "";
-		Scanner entradaEscaner = new Scanner (System.in);
-		entradaTeclado = entradaEscaner.nextLine();
+		String entradaTeclado = "";// Definimos donde se guardaran los datos ingresados
+		Scanner entradaEscaner = new Scanner (System.in);// Se define el scanner para recibir datos
+		entradaTeclado = entradaEscaner.nextLine();// Esta linea recibe datos al usuario
 		
-		System.out.println("Hola " + entradaTeclado + ", ¿Como estas?");
+		System.out.println("Hola " + entradaTeclado + ", ¿Como estas?");// Saluda al nombre asignado
 	}
-}
+}// Se cierra la clase principal
